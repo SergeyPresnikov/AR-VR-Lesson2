6b89589d8af7a7d4cb50841628ee71c8f5999b3e
SergeyPresnikov <83859290+SergeyPresnikov@users.noreply.github.com>
2021-06-05 16:06:24 +1000

    Add files via upload

---
+++ Lesson 1/Asteroids/Asteroid.cs
@@ -0,0 +1,48 @@
+﻿using Asteroids.Properties;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asteroids
+{
+    class Asteroid
+    {
+        protected Point Pos;
+        protected Point Dir;
+        protected Size Size;
+
+        public Asteroid(Point pos, Point dir, Size size)
+        {
+            Pos = pos;
+            Dir = dir;
+            Size = size;
+        }
+
+        public virtual void Draw()
+        {
+            Game.Buffer.Graphics.DrawImage(Resources.meteorBrown_big2, Pos.X, Pos.Y, Size.Width, Size.Height);
+
+            Game.Buffer.Graphics.DrawImage(Resources.meteorBrown_big4, Pos.Y, Pos.X, Size.Width, Size.Height);
+
+        }
+
+
+        public virtual void Update()
+        {
+            Pos.X = Pos.X + Dir.X;
+            Pos.Y = Pos.Y + Dir.Y;
+
+            if (Pos.X < 0) Dir.X = -Dir.X;
+            if (Pos.X > Game.Width) Dir.X = -Dir.X;
+
+            if (Pos.Y < 0) Dir.Y = -Dir.Y;
+            if (Pos.Y > Game.Height) Dir.Y = -Dir.Y;
+
+        }
+
+
+    }
+}
---
+++ Lesson 1/Asteroids/Star.cs
@@ -0,0 +1,39 @@
+﻿using Asteroids.Properties;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asteroids
+{
+    class Star : Asteroid
+    {
+       
+        public Star (Point pos, Point dir, Size size) : base(pos, dir, size)
+        {
+          
+        }
+
+        public override void Draw()
+        {
+
+            Game.Buffer.Graphics.DrawImage(Resources.star2, Pos.X + Size.Height, Pos.Y + Size.Width);
+
+            Game.Buffer.Graphics.DrawImage(Resources.star1, Pos.Y + Size.Width, Pos.X + Size.Height);
+        }
+
+        public override void Update()
+        {
+            Pos.X = Pos.X + Dir.X;
+            Pos.Y = Pos.Y + Dir.Y;
+
+            if (Pos.X < 0) Dir.X = -Dir.X;
+            if (Pos.X > Game.Width) Dir.X = -Dir.X;
+
+            if (Pos.Y < 0) Dir.Y = -Dir.Y;
+            if (Pos.Y > Game.Height) Dir.Y = -Dir.Y;
+        }
+    }
+}
