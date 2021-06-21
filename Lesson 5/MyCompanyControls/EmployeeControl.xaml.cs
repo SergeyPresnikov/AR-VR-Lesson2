be166d3a2263bf0332a2f5d0669a278c08d632e3
SergeyPresnikov <83859290+SergeyPresnikov@users.noreply.github.com>
2021-06-21 19:47:15 +1000

    Add files via upload

---
+++ Lesson 5/MyCompanyControls/EmployeeControl.xaml.cs
@@ -0,0 +1,56 @@
+﻿using MyCompony.Employee;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MyCompanyControls
+{
+    /// <summary>
+    /// Логика взаимодействия для EmployeeControl.xaml
+    /// </summary>
+    public partial class EmployeeControl : UserControl
+    {
+        private Employee employee;
+        public EmployeeControl()
+        {
+            InitializeComponent();
+            cbDepartment.ItemsSource = Enum.GetValues(typeof(DepartmentCategory)).Cast<DepartmentCategory>();
+        }
+
+        public void SetContact(Employee employee)
+        {
+            this.employee = employee;
+
+
+            tbFirstName.Text = employee.FirstName;
+            tbLastName.Text = employee.LastName;
+            tbSecondName.Text = employee.SecondName;
+            cbDepartment.SelectedItem = employee.Category;
+
+        }
+
+        public void UpdateContact()
+        {
+
+            employee.FirstName = tbFirstName.Text;
+            employee.LastName = tbLastName.Text;
+            employee.SecondName = tbSecondName.Text;
+            employee.Category = (DepartmentCategory)cbDepartment.SelectedItem;
+
+
+
+
+        }
+    }
+}
---
+++ Lesson 5/MyCompony.Employee/Employee.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCompony.Employee
+{
+    
+
+    public class Employee
+    {
+
+        /// <summary>
+        /// Имя
+        /// </summary>
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// Фамилия
+        /// </summary>
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// Отчество
+        /// </summary>
+        public string SecondName { get; set; }
+
+        public string FIO
+        {
+            get { return $"{LastName} {FirstName} {SecondName}"; }
+        }
+
+
+        /// <summary>
+        /// Категория
+        /// </summary>
+        public DepartmentCategory Category { get; set; } = DepartmentCategory.Терапевтическое;
+
+        public Employee() { }
+
+        public Employee(string lastName, string firstName, string secondName,  DepartmentCategory category)
+        {
+            LastName = lastName;
+            FirstName = firstName;
+            SecondName = secondName;
+            Category = category;
+        }
+
+        public override string ToString()
+        {
+            return $"{LastName} {FirstName} {SecondName}";
+        }
+    }
+}
+
