760db27e099ea7897d6ce6655c1cda17764850ef
SergeyPresnikov <83859290+SergeyPresnikov@users.noreply.github.com>
2021-06-23 20:40:23 +1000

    Add files via upload

---
+++ Lesson 6/MyCompany/MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using MyCompony.Employee;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MyCompany
+{
+   
+
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private EmployeeBase employeeBase = new EmployeeBase();
+
+        public ObservableCollection<Employee> EmployeeList { get; set; }
+
+        public Employee SelectedEmployee { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.DataContext = this;
+
+            EmployeeList = employeeBase.Staff;
+
+        }
+
+        private void MyCompanyListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (e.AddedItems.Count != 0)
+            {
+                employeeControl.Employee = (Employee)SelectedEmployee.Clone();
+            }
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            EmployeeEditor editor = new EmployeeEditor();
+            if (editor.ShowDialog() == true)
+            {
+                employeeBase.Staff.Add(editor.Employee);
+                MyCompanyListView.SelectedItem = editor.Employee;
+                MyCompanyListView.ScrollIntoView(editor.Employee);
+            }
+        }
+
+        private void btnRemove_Click(object sender, RoutedEventArgs e)
+        {
+            if (MyCompanyListView.SelectedItems.Count < 1)
+                return;
+
+            if (MessageBox.Show("Вы действительно желаете удалить?", "Удалить", MessageBoxButton.YesNo, MessageBoxImage.Question) ==
+                 MessageBoxResult.Yes)
+            {
+                employeeBase.Staff.Remove((Employee)MyCompanyListView.SelectedItems[0]);
+            }
+        }
+
+        private void btnApply_Click(object sender, RoutedEventArgs e)
+        {
+            if (MyCompanyListView.SelectedItems.Count < 1)
+                return;
+
+            EmployeeList[EmployeeList.IndexOf(SelectedEmployee)] = employeeControl.Employee;
+            
+        }
+      
+    }
+}
