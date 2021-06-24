c7cb31bb699ac746bd3586699c1283caf26ddbc8
SergeyPresnikov <83859290+SergeyPresnikov@users.noreply.github.com>
2021-06-24 22:51:03 +1000

    Add files via upload

---
+++ Lesson 5/MyCompony.Employee/DepartmentCategory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCompony.Employee
+{
+    public enum DepartmentCategory
+    {
+        Терапевтическое = 0,
+        Неврологическое = 1,
+        Инфекционное = 2,
+        Урологическое = 3,
+        Хирургическое = 4,
+
+    }
+}
