cb357b1ae17026355ba3b7430ebffa1dc26588db
SergeyPresnikov <83859290+SergeyPresnikov@users.noreply.github.com>
2021-06-28 22:10:20 +1000

    Add files via upload

---
+++ Lesson 7/MyCompany/EmployeeBase.cs
@@ -0,0 +1,106 @@
+﻿using MyCompony.Employee;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCompany
+{
+    public class EmployeeBase
+    {
+
+        public const string ConnectionString = "Data Source=HOME-PS\\SQLEXPRESS;Initial Catalog=MyCompanyPSA;User ID=MyCompanyPSA;Password=123456";
+
+        private static int CHAR_BOUND_L = 65; // Номер начального символа (для генерации последовательности символов)
+        private static int CHAR_BOUND_H = 90; // Номер конечного  символа (для генерации последовательности символов)
+        private Random random = new Random();
+
+        public ObservableCollection<Employee> Staff { get; set; }
+
+
+        public EmployeeBase()
+        {
+            Staff = new ObservableCollection<Employee>();
+            LoadFromEmployeeBase();
+        }
+
+
+        public int Add(Employee employee)
+        {
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+                string sqlExpression = $@"INSERT INTO Staff (LastName, FirstName, SecondName, CategoryID)
+                    VALUES('{employee.LastName}', '{employee.FirstName}', '{employee.SecondName}',{(int)employee.Category})";
+                var command = new SqlCommand(sqlExpression, connection);
+                var res = command.ExecuteNonQuery();
+                if (res > 0)
+                {
+                    Staff.Add(employee);
+                }
+                return res;
+            }
+        }
+
+
+        public int Update(Employee employee)
+        {
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+
+                string sqlExpression = $@"UPDATE Staff
+                    SET FirstName ='{employee.FirstName}', SecondName = '{employee.SecondName}', CategoryId = {(int)employee.Category}
+                    WHERE LastName = '{employee.LastName}'";
+                var command = new SqlCommand(sqlExpression, connection);
+                return command.ExecuteNonQuery();
+            }
+        }
+
+        public int Remove(Employee employee)
+        {
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+
+                string sqlExpression = $@"DELETE FROM Staff WHERE LastName = '{employee.LastName}'";
+                var command = new SqlCommand(sqlExpression, connection);
+                var res = command.ExecuteNonQuery();
+                if (res > 0)
+                {
+                    Staff.Remove(employee);
+                }
+                return res;
+            }
+        }
+
+        public void LoadFromEmployeeBase()
+        {
+            string sqlExpression = "SELECT * FROM Staff";
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+
+                var command = new SqlCommand(sqlExpression, connection);
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+
+                    while (reader.Read())
+                    {
+                        var employee = new Employee();
+                        employee.LastName = reader["LastName"].ToString();
+                        employee.FirstName = reader["FirstName"].ToString();
+                        employee.SecondName = reader["SecondName"].ToString();
+                        employee.Category = (DepartmentCategory)reader.GetInt32(3);
+                        Staff.Add(employee);
+                    }
+                }
+            }
+        }      
+    }
+}
---
+++ Lesson 7/MyCompany/EmployeeEditor.xaml.cs
@@ -0,0 +1,42 @@
+﻿using MyCompony.Employee;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MyCompany
+{
+    /// <summary>
+    /// Логика взаимодействия для EmployeeEditor.xaml
+    /// </summary>
+    public partial class EmployeeEditor : Window
+    {
+        public Employee Employee { get; set; } = new Employee();
+
+        public EmployeeEditor()
+        {
+            InitializeComponent();
+            employeeControl.Employee = Employee;
+        }
+
+       
+        private void btnOK_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
